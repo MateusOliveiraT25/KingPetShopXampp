08abbab891cd4c760ea5e538868383894c3eaf1e
MateusOliveiraT25 <123783867+MateusOliveiraT25@users.noreply.github.com>
2023-11-27 14:49:10 -0300

    27/11

--- cep.js
+++ cep.js
@@ -1,22 +1,14 @@
- function consultarCEP() {
-        var cep = $('#cep').val();
-
-        // Verificar se o CEP possui formato válido (apenas números)
-        if (/^\d{8}$/.test(cep)) {
-            // Consulta à API dos Correios
-            $.getJSON(`https://viacep.com.br/ws/${cep}/json/`, function (data) {
-                if (!("erro" in data)) {
-                    // Preencher os campos com as informações obtidas
-                    $('#resultadoCEP').html(
-                        `<p><strong>Logradouro:</strong> ${data.logradouro}</p>` +
+               `<p><strong>Logradouro:</strong> ${data.logradouro}</p>` +
                         `<p><strong>Bairro:</strong> ${data.bairro}</p>` +
                         `<p><strong>Cidade/UF:</strong> ${data.localidade}/${data.uf}</p>`
                     );
                 } else {
+                    $('#endereco').val(''); // Limpar o campo de endereço em caso de erro
                     $('#resultadoCEP').html('<p>CEP não encontrado</p>');
                 }
             });
         } else {
+            $('#endereco').val(''); // Limpar o campo de endereço em caso de formato inválido
             $('#resultadoCEP').html('<p>Formato de CEP inválido</p>');
         }
     }
