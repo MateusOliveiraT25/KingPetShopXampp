8b20a21c645937e0bd75a893bf281f6af1cab500
MateusOliveiraT25 <oliveiramateus1222@gmail.com>
2023-12-07 20:12:08 -0300

    07/12

--- produtos.php
+++ produtos.php
@@ -12,6 +12,13 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="herder_footer.js"></script> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <style>
+        /* Defina o tamanho fixo para as imagens dos produtos */
+        .product-image {
+            width: 100%; /* Tamanho desejado em relação ao contêiner pai */
+            height: auto; /* Mantém a proporção original da imagem */
+        }
+    </style>
 </head>
 <body>
     
