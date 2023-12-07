c642dcfb5ae0b847dc806a8d9dd5a19565d6c5a4
oliveiramarcelo12 <oliveiramarcelo198@gmail.com>
2023-12-07 20:53:54 -0300

    07/12/2023

--- remover_produto.php
+++ remover_produto.php
@@ -1,20 +1,40 @@
 <?php
-session_start();
+// Verificar se o formulário foi enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Conectar ao banco de dados
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "king";
+    $conn = new mysqli($servername, $username, $password, $database);
 
-if (isset($_GET['index'])) {
-    $index = $_GET['index'];
-
-    // Verifique se o índice existe no carrinho
-    if (isset($_SESSION['carrinho'][$index])) {
-        // Remova o produto do carrinho usando o índice
-        unset($_SESSION['carrinho'][$index]);
-
-        // Redirecione de volta para a página do carrinho
-        header("Location: mostrar_carrinho.php");
-        exit();
+    // Verificar a conexão
+    if ($conn->connect_error) {
+        die("Falha na conexão com o banco de dados: " . $conn->connect_error);
     }
-}
 
-// Redirecione em caso de falha ou índice ausente
-header("Location: carrinho.php");
-exit();
+    // Verificar se o campo do produto a ser removido está presente no formulário
+    if (isset($_POST['produto_id'])) {
+        $produto_id = $_POST['produto_id'];
+
+        // Debug: Exibir o ID do produto para verificar se está correto
+        echo "Produto ID: " . $produto_id;
+
+        // Preparar e executar a consulta SQL para remover o produto
+        $sql = "DELETE FROM produtos WHERE id = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("i", $produto_id);
+
+        if ($stmt->execute()) {
+            echo "Produto removido com sucesso!";
+        } else {
+            echo "Erro ao remover produto: " . $stmt->error;
+        }
+
+        $stmt->close();
+    }
+
+    // Fechar a conexão com o banco de dados
+    $conn->close();
+}
+?>
