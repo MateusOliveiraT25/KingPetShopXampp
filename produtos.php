9dbf5e18e07353a66d0ef071b93215a1dd512cc4
MateusOliveiraT25 <oliveiramateus1222@gmail.com>
2023-12-07 21:06:32 -0300

    07/12

--- produtos.php
+++ produtos.php
@@ -11,6 +11,7 @@
     <script src="script.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <style>
         /* Defina o tamanho fixo para as imagens dos produtos */
         .product-image {
@@ -18,17 +19,22 @@
             height: auto; /* Mantém a proporção original da imagem */
         }
     </style>
+    <script>
+      // Use jQuery para carregar o cabeçalho em todas as páginas
+      $(function () {
+        $("#header").load("header.php");
+      });
+      $(function () {
+        $("#footer").load("footer.html");
+      });
+    </script>
 </head>
 <body>
 
     <!-- Navegação -->
     <nav>
-        <ul>
-            <li><a href="index.html">Página Inicial</a></li>
-            <li><a href="cadastro.html">Cadastro</a></li>
-            <li><a href="login.html">Login</a></li>
-            <!-- Adicione mais links conforme necessário -->
-        </ul>
+         <!-- header -->
+    <div id="header"></div>
     </nav>
 
     <!-- Conteúdo da Página -->
@@ -131,7 +137,10 @@ $conn->close();
         }
     });
 }
+
 </script>
 
+    <!-- Rodapé da Página -->
+    <div id="footer"></div>
 </body>
 </html>
